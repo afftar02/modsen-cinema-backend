b0ba0fee4973a0baf13c500c19e754fafd3d496e
Steckij Nikita <stets119@gmail.com>
2023-09-12 10:32:20 +0300

    cors resolved

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  app.enableCors({ credentials: true, origin: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
 }
